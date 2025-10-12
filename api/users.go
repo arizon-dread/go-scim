bbef5d698f66a9a595ac241bc463e24ae0d26503
Erik Svensson <arizon.es@gmail.com>
2025-10-12 23:25:21 +0200

    add patch func + handler

--- api/users.go
+++ api/users.go
@@ -1,6 +1,8 @@
 package api
 
 import (
+	"encoding/json"
+	"go-scim/pkg/models"
 	"net/http"
 )
 
@@ -10,6 +12,25 @@ func GetUsers(w http.ResponseWriter, r *http.Request) {
 	if id == "" {
 		w.WriteHeader(http.StatusBadRequest)
 		w.Write([]byte("unable to read body"))
+		return
+	}
+
+}
+
+func PatchUsers(w http.ResponseWriter, r *http.Request) {
+	var b []byte
+	_, err := r.Body.Read(b)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to read body"))
+		return
+	}
+	var users []models.User
+	err = json.Unmarshal(b, &users)
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Unable to marshal payload into go struct"))
+		return
 	}
 
 }
