54eb578dc39c8f9c43da2ce564d6e83ed399ebb8
Erik Svensson <arizon.es@gmail.com>
2025-10-16 23:06:16 +0200

    some error handling in the handler

--- pkg/handlers/userHandler.go
+++ pkg/handlers/userHandler.go
@@ -1,1 +1,25 @@
 package handlers
+
+import (
+	"go-scim/internal/repository"
+	"go-scim/pkg/errors"
+	"go-scim/pkg/models"
+	"log"
+)
+
+func UpsertUsers(users []models.User) error {
+
+	for _, v := range users {
+		if err := repository.UpsertUser(v); err != nil {
+			e := errors.ErrorType{
+				Type:    1,
+				Code:    500,
+				Message: "Unable to update user in db",
+				Err:     err,
+			}
+			log.Printf("user not upserted, id: %v, externalId: %v, reason: %w", v.ID, v.ExternalID, e)
+			return err
+		}
+	}
+	return nil
+}
