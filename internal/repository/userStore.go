2664be3f5c1ed86c287e52c2b9f7e2ffae3c4873
Erik Svensson <arizon.es@gmail.com>
2025-10-16 23:05:02 +0200

    update userstore with func skeleton

--- internal/repository/userStore.go
+++ internal/repository/userStore.go
@@ -3,5 +3,11 @@ package repository
 import "go-scim/pkg/models"
 
 func GetUser(id string) (models.User, error) {
+
 	return models.User{}, nil
+}
+
+func UpsertUser(user models.User) error {
+
+	return nil
 }
